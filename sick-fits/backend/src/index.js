ef1d4d3da7cbc73cbc00524d2a33d259c4424e5d
Owen Stowe <ostowe@gmail.com>
2020-05-05 18:53:35 -0400

    Complete module 3

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+const path = require('path');
+require('dotenv').config({ path: path.join(process.cwd(), '.env') });
+const createServer = require('./createServer');
+const db = require('./db');
+const server = createServer();
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, (serverInfo) => {
+  console.log(`Server is running on https://localhost:${serverInfo.port}`);
+});
